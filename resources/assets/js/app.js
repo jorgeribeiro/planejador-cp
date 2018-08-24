ff92becf5c613636a812a717a53cc15d35928eb7
Jorge Ribeiro <joorgemelo@gmail.com>
2018-08-24 13:56:43 -0300

    First page created. Controller to create

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,13 +9,18 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+Vue.use(Buefy)
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('welcome', require('./components/Welcome.vue'));
+Vue.component('primeiros_passos', require('./components/PrimeirosPassos.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/primeiros_passos.blade.php
+++ resources/views/primeiros_passos.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
@@ -6,15 +6,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>        
+        <title>Planejador CP (UFMA)</title>
 
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     </head>
     <body>
         <div id="app">
-            <welcome :title="'This cool app'"></welcome>            
+            <primeiros_passos></primeiros_passos>            
         </div>
+        
         <script type="text/javascript" src="js/app.js"></script>
     </body>
 </html>
